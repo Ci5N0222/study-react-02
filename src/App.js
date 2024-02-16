45b3b6c7d4f88712434972988866750f8a310d3d
C-ON <vmfpsel@gmail.com>
2024-02-16 21:49:22 +0900

    setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 import { Container, Row, Col, Nav, Navbar, Button } from 'react-bootstrap';
+import productData from "./data.js";
 
 function App() {
+
+  let [shoes] = useState(productData);
+
   return (
     <div className="App">
+      <Navigation />
+      <div className="main-bg"></div>
+      <Product product={productData}/>
+    </div>
+  );
+}
+
+// Navigation
+const Navigation = () => {
+  return(
+    <div>
       <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="#home">ShoseMall</Navbar.Brand>
@@ -17,19 +29,35 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg"></div>
-      <div>
-        <Container>
+    </div>
+  )
+}
+
+// Main page product
+const Product = (props) => {
+  return(
+    <div>
+      <Container>
         <Row>
-          <Col sm>sm=true</Col>
-          <Col sm>sm=true</Col>
-          <Col sm>sm=true</Col>
+          <Col sm={4}>
+            <img src={ props.product[0].img } width="100%" />
+            <h4>{ props.product[0].title }</h4>
+            <p>{ props.product[0].content }</p>
+          </Col>
+          <Col sm={4}>
+            <img src={ props.product[1].img } width="100%" />
+            <h4>{ props.product[1].title }</h4>
+            <p>{ props.product[1].content }</p>
+          </Col>
+          <Col sm={4}>
+            <img src={ props.product[2].img } width="100%" />
+            <h4>{ props.product[2].title }</h4>
+            <p>{ props.product[2].content }</p>
+          </Col>
         </Row>
       </Container>
-      </div>
-      
     </div>
-  );
+  )
 }
 
 export default App;
