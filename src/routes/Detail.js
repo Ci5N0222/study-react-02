3b8bf371c44c2638ac8ad1f303fdb23a59b93c4e
C-ON <vmfpsel@gmail.com>
2024-02-20 15:00:23 +0900

    setup

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,17 @@
-const Detail = () => {
+const Detail = (props) => {
     return(
-        <div>
-            
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <img src={ props.shoes[0].img } width="100%"/>
+                </div>
+                <div className="col-md-6 mt-4">
+                    <h4 className="pt-5">{ props.shoes[0].title }</h4>
+                    <p>{ props.shoes[0].content }</p>
+                    <p>{ props.shoes[0].price }원</p>
+                    <button className="btn btn-danger">주문하기</button>
+                </div>
+            </div>
         </div>
     )
 }
